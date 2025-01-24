e6210e86ec20860e4317411ef657932f63fdee03
Marco Macherelli <elmache05@gmail.com>
2025-01-24 16:54:23 +0100

    Build fix

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+import { heroui } from "@heroui/react";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    heroui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: "#0a0a0a",
+              hover: "#727272",
+            },
+          },
+        },
+      },
+    }),
+  ],
+};
