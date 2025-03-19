13f83ac93e38a72ce6fb93b87f18eccec43212f5
Marco Macherelli <elmache05@gmail.com>
2025-03-19 20:01:46 +0100

    View Premium Customers

--- src/Components/Campaigns/Other/AddCampaignModel.tsx
+++ src/Components/Campaigns/Other/AddCampaignModel.tsx
@@ -1,70 +1,115 @@
-// AddContactModel.tsx
-"use client"
+import { useEffect, useState } from "react";
+import { useDisclosure } from "@heroui/react";
+import { Button } from "@heroui/react";
+import axios from "axios";
+import type {
+  EmailCampaign,
+  WhatsappCampaign,
+  Contact,
+  Cap,
+} from "./types/campaign";
+import {
+  EMAILCAMPAIGN_INITIAL,
+  WHATSAPPCAMPAIGN_INITIAL,
+} from "./utils/constants";
+import { CustomerTypeSelector } from "./components/campaign/CustomerTypeSelector";
+import { CampaignTypeSelector } from "./components/campaign/CampaignTypeSelector";
+import { ContactSelector } from "./components/campaign/ContactSelector";
+import { CampaignForm } from "./components/campaign/CampaignForm";
+import { ConfirmationModal } from "./components/campaign/ConfirmationModal";
+import { PreviewMailModal, PreviewWhatsAppModal } from "./preview-components";
 
-import { useEffect, useState } from "react"
-import { useDisclosure } from "@heroui/react"
-import { Button } from "@heroui/react"
-import axios from "axios"
-import type { EmailCampaign, WhatsappCampaign, Contact, Cap } from "./types/campaign"
-import { EMAILCAMPAIGN_INITIAL, WHATSAPPCAMPAIGN_INITIAL } from "./utils/constants"
-import { CustomerTypeSelector } from "./components/campaign/CustomerTypeSelector"
-import { CampaignTypeSelector } from "./components/campaign/CampaignTypeSelector"
-import { ContactSelector } from "./components/campaign/ContactSelector"
-import { CampaignForm } from "./components/campaign/CampaignForm"
-import { ConfirmationModal } from "./components/campaign/ConfirmationModal"
-import { PreviewMailModal, PreviewWhatsAppModal } from "./preview-components"
+interface Employee {
+  EmployeeId: number;
+  StafferName: string;
+  StafferSurname: string;
+  EmployeeEmail: string;
+  EmployeePhone: string;
+  StafferImageUrl: string;
+  Agente: string;
+}
+
+const USERDATA_VALUE: Employee = {
+  EmployeeId: 0,
+  StafferName: "",
+  StafferSurname: "",
+  EmployeeEmail: "",
+  EmployeePhone: "",
+  StafferImageUrl: "",
+  Agente: "",
+};
 
 export default function AddContactModel() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure()
-  const [customerType, setCustomerType] = useState("private")
-  const [campaignType, setCampaignType] = useState("email")
-  const [emailCampaign, setEmailCampaign] = useState<EmailCampaign>(EMAILCAMPAIGN_INITIAL)
-  const [whatsappCampaign, setWhatsappCampaign] = useState<WhatsappCampaign>(WHATSAPPCAMPAIGN_INITIAL)
-  const [images, setImages] = useState<FileList | null>(null)
-  const [previewOpen, setPreviewOpen] = useState(false)
-  const [whatsAppPreviewOpen, setWhatsAppPreviewOpen] = useState(false)
-  const [selectedCaps, setSelectedCaps] = useState<Cap[]>([])
-  const [selectedContacts, setSelectedContacts] = useState<Contact[]>([])
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [isWhatsappBlock, setIsWhatsappBlock] = useState({ blocked: false })
-
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [customerType, setCustomerType] = useState("private");
+  const [campaignType, setCampaignType] = useState("email");
+  const [emailCampaign, setEmailCampaign] = useState<EmailCampaign>(
+    EMAILCAMPAIGN_INITIAL
+  );
+  const [whatsappCampaign, setWhatsappCampaign] = useState<WhatsappCampaign>(
+    WHATSAPPCAMPAIGN_INITIAL
+  );
+  const [images, setImages] = useState<FileList | null>(null);
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [whatsAppPreviewOpen, setWhatsAppPreviewOpen] = useState(false);
+  const [selectedCaps, setSelectedCaps] = useState<Cap[]>([]);
+  const [selectedContacts, setSelectedContacts] = useState<Contact[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isWhatsappBlock, setIsWhatsappBlock] = useState({ blocked: false });
+  const [userData, setUserData] = useState<Employee>(USERDATA_VALUE);
   const handleEmailCampaignInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setEmailCampaign((prevData) => ({ ...prevData, [name]: value }))
-  }
+    const { name, value } = e.target;
+    setEmailCampaign((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-  const handleWhatsappCampaignInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setWhatsappCampaign((prevData) => ({ ...prevData, [name]: value }))
-  }
+  const handleWhatsappCampaignInput = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = e.target;
+    setWhatsappCampaign((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   const handleBothInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setEmailCampaign((prevData) => ({ ...prevData, [name]: value }))
-    setWhatsappCampaign((prevData) => ({ ...prevData, [name]: value }))
-  }
+    const { name, value } = e.target;
+    setEmailCampaign((prevData) => ({ ...prevData, [name]: value }));
+    setWhatsappCampaign((prevData) => ({ ...prevData, [name]: value }));
+  };
 
   const handleImageInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
-      setImages(e.target.files)
+      setImages(e.target.files);
     }
-  }
+  };
 
   function handleRadioValueChange(e: string) {
-    setEmailCampaign(EMAILCAMPAIGN_INITIAL)
-    setWhatsappCampaign(WHATSAPPCAMPAIGN_INITIAL)
-    setImages(null)
-    setCampaignType(e)
+    setEmailCampaign(EMAILCAMPAIGN_INITIAL);
+    setWhatsappCampaign(WHATSAPPCAMPAIGN_INITIAL);
+    setImages(null);
+    setCampaignType(e);
   }
+
+  useEffect(() => {
+    axios
+      .get("/Authentication/GET/GetSessionData", { withCredentials: true })
+      .then((res) => {
+        setUserData(res.data);
+        setSelectedContacts([
+          {
+            code: res.data.Agente,
+            name: res.data.StafferName + " " + res.data.StafferSurname,
+          },
+        ]);
+      });
+  }, []);
 
   async function handleSubmit() {
     try {
-      setIsLoading(true)
-      const CustomerData = new FormData()
+      setIsLoading(true);
+      const CustomerData = new FormData();
 
       // Add campaign type and customer type
-      CustomerData.append("CampaignType", campaignType)
-      CustomerData.append("ContactType", customerType)
+      CustomerData.append("CampaignType", campaignType);
+      CustomerData.append("ContactType", customerType);
 
       // Add campaign data based on type
       if (campaignType === "email" || campaignType === "both") {
@@ -74,59 +119,78 @@ export default function AddContactModel() {
           emailCampaign.Description === "" ||
           images === null
         ) {
-          alert("Compila tutti i campi")
-          return
+          alert("Compila tutti i campi");
+          return;
         }
-        CustomerData.append("EmailCampaignData", JSON.stringify(emailCampaign))
+        CustomerData.append("EmailCampaignData", JSON.stringify(emailCampaign));
       }
 
       if (campaignType === "whatsapp" || campaignType === "both") {
-        if (whatsappCampaign.Title === "" || whatsappCampaign.Description === "" || images === null) {
-          alert("Compila tutti i campi")
-          return
+        if (
+          whatsappCampaign.Title === "" ||
+          whatsappCampaign.Description === "" ||
+          images === null
+        ) {
+          alert("Compila tutti i campi");
+          return;
         }
-        CustomerData.append("WhatsappCampaignData", JSON.stringify(whatsappCampaign))
+        CustomerData.append(
+          "WhatsappCampaignData",
+          JSON.stringify(whatsappCampaign)
+        );
       }
 
       // Add images
       if (images) {
         Array.from(images).forEach((image) => {
-          CustomerData.append("Images", image)
-        })
+          CustomerData.append("Images", image);
+        });
       }
 
       // Add selected CAPs
-      CustomerData.append("SelectedCaps", JSON.stringify(selectedCaps))
+      CustomerData.append("SelectedCaps", JSON.stringify(selectedCaps));
 
       // Add selected contacts
-      CustomerData.append("SelectedContacts", JSON.stringify(selectedContacts))
+      CustomerData.append("SelectedContacts", JSON.stringify(selectedContacts));
 
-      const res = await axios.post("/Campaigns/POST/AddNewCampaign", CustomerData)
+      const res = await axios.post(
+        "/Campaigns/POST/AddNewCampaign",
+        CustomerData
+      );
       if (res.status === 201) {
-        window.location.href = "/Campaigns"
+        window.location.href = "/Campaigns";
       }
     } catch (error) {
-      setIsLoading(false)
-      console.error("Si è verificato un errore:", error)
+      setIsLoading(false);
+      console.error("Si è verificato un errore:", error);
     }
   }
 
+  console.log(selectedContacts);
+
   useEffect(() => {
     async function checkWhatsappBlock() {
-      const res = await axios.get("/Campaigns/GET/CheckWhatsappBlock")
+      const res = await axios.get("/Campaigns/GET/CheckWhatsappBlock");
       if (res.status === 200) {
-        setIsWhatsappBlock(res.data)
+        setIsWhatsappBlock(res.data);
       }
     }
-    checkWhatsappBlock()
-  }, [])
+    checkWhatsappBlock();
+  }, []);
 
   return (
     <form className="relative">
-      {(campaignType === "email" || campaignType === "both" || campaignType === "whatsapp") && (
+      {(campaignType === "email" ||
+        campaignType === "both" ||
+        campaignType === "whatsapp") && (
         <div className="w-full flex flex-row justify-end gap-2">
           {(campaignType === "email" || campaignType === "both") && (
-            <Button type="button" color="primary" size="lg" onClick={() => setPreviewOpen(true)}>
+            <Button
+              type="button"
+              color="primary"
+              size="lg"
+              onClick={() => setPreviewOpen(true)}
+            >
               👀 Anteprima Email
             </Button>
           )}
@@ -146,7 +210,10 @@ export default function AddContactModel() {
 
       <div className="flex flex-col gap-5 space-y-12">
         <div className="flex flex-row gap-5 sm:col-span-4">
-          <CustomerTypeSelector value={customerType} onChange={setCustomerType} />
+          <CustomerTypeSelector
+            value={customerType}
+            onChange={setCustomerType}
+          />
           <CampaignTypeSelector
             value={campaignType}
             onChange={handleRadioValueChange}
@@ -201,11 +268,11 @@ export default function AddContactModel() {
               setEmailCampaign((prevData) => ({
                 ...prevData,
                 Description: value,
-              }))
+              }));
               setWhatsappCampaign((prevData) => ({
                 ...prevData,
                 Description: value,
-              }))
+              }));
             }}
             onImageChange={handleImageInput}
             allowMultipleImages
@@ -222,7 +289,12 @@ export default function AddContactModel() {
         </Button>
       </div>
 
-      <ConfirmationModal isOpen={isOpen} onOpenChange={onOpenChange} onConfirm={handleSubmit} isLoading={isLoading} />
+      <ConfirmationModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        onConfirm={handleSubmit}
+        isLoading={isLoading}
+      />
 
       {(campaignType === "email" || campaignType === "both") && (
         <PreviewMailModal
@@ -249,5 +321,5 @@ export default function AddContactModel() {
         />
       )}
     </form>
-  )
+  );
 }
